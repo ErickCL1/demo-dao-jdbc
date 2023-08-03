cffae91562085973f9c031b6ffd46504d5a76dea
ErickCL1 <erickcordeiro23@gmail.com>
2023-08-03 16:23:04 -0300

    criação do findAll

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
-import java.util.Date;
+
+
+
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -17,6 +21,21 @@ public class Program {
 		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
+		
+		System.out.println("=== TESTE2: seller findByDepartment ===");
+		
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for (Seller obj:list) {
+			System.out.println(obj);
+		}
+		
+		System.out.println("=== TESTE3: seller findAll ===");
+		list = sellerDao.findAll();
+		for (Seller obj:list) {
+			System.out.println(obj);
+		}
+		
 		
 	}
 
